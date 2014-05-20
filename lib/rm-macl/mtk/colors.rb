59e7e0adbba5b61fb30aa6fd5983fc14dd784be9
IceDragon200 <mistdragon100@gmail.com>
2014-05-20 12:15:52 -0500

    MACL Toolkit, colors

---
+++ lib/rm-macl/mtk/colors.rb
@@ -0,0 +1,8 @@
+#
+# rm-macl/mtk/colors.rb
+#
+require "rm-macl/fallback/color"
+require "rm-macl/mixin/color_math"
+require "rm-macl/xpan/color_vector"
+require "rm-macl/rgss3-ext/color"
+require "rm-macl/xpan/palette"
